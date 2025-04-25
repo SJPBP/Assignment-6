ccad7f1618f25c7de2764322e1495ead3a4f17bb
SJPBP <73006594+SJPBP@users.noreply.github.com>
2025-04-25 16:18:52 -0400

    Added key and traction

--- server/server.go
+++ server/server.go
@@ -11,19 +11,6 @@ import (
 	"time"
 )
 
-// Struct for key used for transaction
-type KEY struct {
-	key                 string
-	committed_value     int
-	committed_timestamp int
-	RTS                 []int
-	TW                  map[int]int
-}
-
-// Create empty store map
-// Store holding x and y variable
-var store = make(map[string]KEY)
-
 type Timestamp int64
 
 type BeginArgs struct {
@@ -71,6 +58,18 @@ type Transaction struct {
 // Create map to store every transaction
 var transaction = make(map[string]*Transaction)
 
+// Struct for key used for transaction
+type KEY struct {
+	committed_value     int
+	committed_timestamp int
+	RTS                 []int
+	TW                  map[Timestamp]int
+}
+
+// Create empty store map
+// Store holding x and y variable
+var store = make(map[string]*KEY)
+
 func (s *TransactionService) BeginTransaction(args *BeginArgs, reply *BeginReply) error {
 	// Get timestamp of current time
 	ts := Timestamp(time.Now().UnixMicro())
@@ -85,6 +84,21 @@ func (s *TransactionService) BeginTransaction(args *BeginArgs, reply *BeginReply
 }
 
 func (s TransactionService) Read(args *ReadArgs, reply *ReadReply) error {
+	tx, ok := transaction[args.TxID]
+	if ok {
+		key, ok := store[args.Key]
+		if ok {
+			// if tx Timestamp > key commited timestamp
+			if tx.Timestamp > key.committed_timestamp {
+				//
+			}
+		} else {
+			reply.Error = "Key not found"
+		}
+	} else {
+		reply.Error = "Transaction not found"
+	}
+
 	return nil
 }
 
@@ -96,77 +110,23 @@ func (s *TransactionService) Commit(args *CommitArgs, reply *CommitReply) error 
 	return nil
 }
 
-// type Backend struct {
-// 	int
-// 	mu   sync.Mutex
-// 	rwMu sync.RWMutex
-// }
-
-// func (t *Backend) Put(args *PutArgs, reply *PutReply) error {
-// 	t.mu.Lock()
-// 	store[args.Key] = args.Value
-//
-// 	_, ok := store[args.Key]
-//
-// 	if ok == true {
-// 		fmt.Printf("\nClient %v: put(%v,%v)\n", args.ClientName, args.Key, args.Value)
-// 		printStore()
-//
-// 		t.mu.Unlock()
-// 		return nil
-// 	}
-// 	t.mu.Unlock()
-//
-// 	return errors.New("Please provide key and value")
-// }
-//
-// func (t *Backend) Get(args *GetArgs, reply *GetReply) error {
-// 	t.rwMu.RLock()
-// 	_, ok := store[args.Key]
-//
-// 	if ok == true {
-// 		reply.Value = store[args.Key]
-//
-// 		fmt.Printf("\nClient %v: get(%v) = %v\n", args.ClientName, args.Key, reply.Value)
-//
-// 		t.rwMu.RUnlock()
-// 		return nil
-// 	}
-// 	fmt.Printf("\nClient %v: get(%v): key not found\n", args.ClientName, args.Key)
-//
-// 	errorMessage := fmt.Sprintf("get(%v): key not found", args.Key)
-//
-// 	t.rwMu.RUnlock()
-// 	return errors.New(errorMessage)
-// }
-//
-// func (t *Backend) Del(args *DelArgs, reply *DelReply) error {
-// 	t.mu.Lock()
-// 	_, ok := store[args.Key]
-//
-// 	if ok == true {
-// 		delete(store, args.Key)
-//
-// 		fmt.Printf("\nClient %v: del(%v)\n", args.ClientName, args.Key)
-// 		t.mu.Unlock()
-// 		return nil
-// 	}
-// 	fmt.Printf("\nClient %v: del(%v): key not found\n", args.ClientName, args.Key)
-//
-// 	errorMessage := fmt.Sprintf("del(%v): key not found", args.Key)
-//
-// 	t.mu.Unlock()
-// 	return errors.New(errorMessage)
-// }
-
 func main() {
 	// Functions aviable
 	transaction := new(TransactionService)
 	rpc.Register(transaction) // Register RPC service
 
 	// Create store with x and y with value of 0
-	store["x"] = 0
-	store["y"] = 0
+	store["x"] = &KEY{} // Create empty KEY
+	store["x"].committed_value = 0
+	store["x"].committed_timestamp = 0
+	store["x"].RTS = append(store["x"].RTS, 0)
+	store["x"].TW = make(map[Timestamp]int)
+
+	store["y"] = &KEY{}
+	store["y"].committed_value = 0
+	store["y"].committed_timestamp = 0
+	store["y"].RTS = append(store["y"].RTS, 0)
+	store["y"].TW = make(map[Timestamp]int)
 
 	listener, err := net.Listen("tcp", ":4000") // Create TCP listener on port 4000
 	if err != nil {
